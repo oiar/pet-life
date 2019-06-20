c6972f1425fac051446519aeb35f9e35f8df6349
oiar <snowoiar@gmail.com>
2019-06-20 18:15:24 +0800

    music

--- lib/pages/service/components/food.dart
+++ lib/pages/service/components/food.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:http/http.dart';
+import 'player_widget.dart';
+
+const kUrl1 = 'https://luan.xyz/files/audio/ambient_c_motion.mp3';
 
 class FoodScreen extends StatefulWidget {
   @override
@@ -7,20 +12,32 @@ class FoodScreen extends StatefulWidget {
 }
 
 class Screen extends State<FoodScreen> {
+
+  AudioPlayer advancedPlayer;
+
+  @override
+  initState() {
+    super.initState();
+    // loadMusic();
+  }
+
+  Future loadMusic() async {
+    advancedPlayer = await AudioCache().play("music/ggg.mp3");
+  }
+
+  @override
+  void dispose() {
+    advancedPlayer = null;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scrollbar(
       child: new SingleChildScrollView(
-        child: FloatingActionButton(onPressed: () {
-          fetchData();
-        }),
+        child: PlayerWidget(url: kUrl1),
+        // child: Container()
       ),
     );
-  }
-
-  void fetchData() {
-    get('https://jsonplaceholder.typicode.com/photos').then((result){
-      print(result.body);
-    });
   }
 }
